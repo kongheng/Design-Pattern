1ffae418488bbc05ef4c1ef487c67a2c76d2139d
kongheng <kongheng.long14@gmail.com>
2021-04-16 19:22:39 +0700

    Template Method Pattern

---
+++ src/main/java/template_method/demo/one/engineering/papers/Electronics.java
@@ -0,0 +1,8 @@
+package template_method.demo.one.engineering.papers;
+
+public class Electronics extends BasicEngineering{
+    @Override
+    public void specialPaper() {
+        System.out.println("Digital Login and Circuit Theory");
+    }
+}
